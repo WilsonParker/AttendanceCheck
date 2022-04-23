a3ff72ec537a7dc3998f0ed1ed738d990b9a782a
Wilson <zerad3208@gmail.com>
2022-04-23 12:11:38 +0900

    Update platforms.php

--- config/platforms.php
+++ config/platforms.php
@@ -9,8 +9,6 @@ function buildDara(string $id, string $pw): array
 }
 
 return [
-
     'yesfile' => buildDara(env('YESFILE_ID'), env('YESFILE_PW')),
     'applefile' => buildDara(env('APPLEFILE_ID'), env('APPLEFILE_PW')),
-
 ];
